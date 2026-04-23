8afa5f7f2bddecbde81ce9a6c9669ecea4a7ea7c
Wu58958 <s103185@gmail.com>
2026-04-23 10:04:46 +0800

    Add files via upload

---
+++ CakeShop/Services/EmailSender.cs
@@ -0,0 +1,38 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace CakeShop.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly IConfiguration _config;
+        public EmailSender(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress("蛋糕訂購網", _config["EmailSettings:SenderEmail"]));
+            emailMessage.To.Add(new MailboxAddress("", email));
+            emailMessage.Subject = subject;
+
+            var bodyBuilder = new BodyBuilder { HtmlBody = htmlMessage };
+            emailMessage.Body = bodyBuilder.ToMessageBody();
+
+            using var client = new SmtpClient();
+            try
+            {
+                await client.ConnectAsync(_config["EmailSettings:SmtpServer"], int.Parse(_config["EmailSettings:Port"]), true);
+                await client.AuthenticateAsync(_config["EmailSettings:Username"], _config["EmailSettings:Password"]);
+                await client.SendAsync(emailMessage);
+            }
+            finally
+            {
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
